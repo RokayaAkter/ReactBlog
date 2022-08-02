ee3a340a37176544d19dfe5d1277e8628bf135a3
unknown <rokayaakter7@gmail.com>
2022-08-02 23:32:11 +0800

    reactwebsite

---
+++ src/Sdata.jsx
@@ -0,0 +1,36 @@
+import culture from "../src/images/s1.jpg";
+import food from "../src/images/food.png";
+import street from "../src/images/street.png";
+import shopping from "../src/images/shopping.jpg";
+import kids from "../src/images/kids.jpg";
+import place from "../src/images/place.png";
+
+
+const Sdata = [
+     {
+          imgsrc: culture,
+          title: "Cultural food",
+     },
+     {
+          imgsrc: food,
+          title: "Taste explorer",
+     },
+     {
+          imgsrc: street,
+          title: "Street Food",
+     },
+     {
+          imgsrc: shopping,
+          title: "Shopping",
+     },
+     {
+          imgsrc: kids,
+          title: "Kids Zone",
+     },
+     {
+          imgsrc: place,
+          title: "Cultural Place",
+     },
+];
+
+export default Sdata;
